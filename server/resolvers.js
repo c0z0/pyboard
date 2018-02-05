92f29665b61b9e589920ea629cb5e404103dd829
Cosmin Serdean <cosmoserdean@gmail.com>
2018-02-05 02:12:38 +0200

    Reset scalar feature

--- server/resolvers.js
+++ server/resolvers.js
@@ -6,45 +6,57 @@ const experiments = [{ name: 'Example', id: '1' }]
 const scalars = [mockscalar]
 
 export default {
-  Query: {
-    allExperiments: () => experiments,
-    experiment: (_, { id }) => experiments.find(i => i.id == id)
-  },
-  Mutation: {
-    createExperiment: (_, { name }) => {
-      const newExperiment = { name, id: shortid.generate() }
-      experiments.push(newExperiment)
-      return newExperiment
-    },
-    createScalar: (_, { name, experimentId }) => {
-      const newScalar = {
-        name,
-        experimentId,
-        values: [],
-        times: [],
-        id: shortid.generate()
-      }
-      scalars.push(newScalar)
-      return newScalar
-    },
-    addScalarValue: (_, { scalarId, value, time }) => {
-      const scalar = scalars.find(s => s.id === scalarId)
-      scalar.values.push(value)
-      scalar.times.push(time)
-      return scalar
-    },
-    addMultipleScalarValues: (_, { scalarId, values, times }) => {
-      const scalar = scalars.find(s => s.id === scalarId)
-      scalar.values.push(...values)
-      scalar.times.push(...times)
-      return scalar
-    }
-  },
-  Experiment: {
-    scalars: ({ id }) => scalars.filter(i => i.experimentId === id)
-  },
-  Scalar: {
-    experiment: ({ experimentId }) =>
-      experiments.find(i => i.id === experimentId)
-  }
+	Query: {
+		allExperiments: () => experiments,
+		experiment: (_, { id }) => experiments.find(i => i.id == id)
+	},
+	Mutation: {
+		createExperiment: (_, { name }) => {
+			const newExperiment = { name, id: shortid.generate() }
+			experiments.push(newExperiment)
+			return newExperiment
+		},
+		createScalar: (_, { name, experimentId }) => {
+			if (scalars.find(s => s.experimentId === experimentId && s.name === name))
+				throw new Error('Name must be unique')
+			const newScalar = {
+				name,
+				experimentId,
+				values: [],
+				times: [],
+				id: shortid.generate()
+			}
+			scalars.push(newScalar)
+			return newScalar
+		},
+		getAndResetScalar: (_, { name, experimentId }) => {
+			const scalar = scalars.find(
+				s => s.experimentId === experimentId && s.name === name
+			)
+
+			scalar.values = []
+			scalar.times = []
+
+			return scalar
+		},
+		addScalarValue: (_, { scalarId, value, time }) => {
+			const scalar = scalars.find(s => s.id === scalarId)
+			scalar.values.push(value)
+			scalar.times.push(time)
+			return scalar
+		},
+		addMultipleScalarValues: (_, { scalarId, values, times }) => {
+			const scalar = scalars.find(s => s.id === scalarId)
+			scalar.values.push(...values)
+			scalar.times.push(...times)
+			return scalar
+		}
+	},
+	Experiment: {
+		scalars: ({ id }) => scalars.filter(i => i.experimentId === id)
+	},
+	Scalar: {
+		experiment: ({ experimentId }) =>
+			experiments.find(i => i.id === experimentId)
+	}
 }
